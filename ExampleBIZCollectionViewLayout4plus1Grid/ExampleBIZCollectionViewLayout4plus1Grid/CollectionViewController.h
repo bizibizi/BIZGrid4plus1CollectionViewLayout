d037f70372452cc1c974ff7192f681585172e0a9
bizibizi <IgorBizi@ms-MacBook-Air.local>
2015-12-11 18:50:58 +0200

    initial

---
+++ ExampleBIZCollectionViewLayout4plus1Grid/ExampleBIZCollectionViewLayout4plus1Grid/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  ExampleBIZCollectionViewLayout4plus1Grid
+//
+//  Created by IgorBizi@mail.ru on 12/11/15.
+//  Copyright © 2015 IgorBizi@mail.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
---
+++ ExampleBIZCollectionViewLayout4plus1Grid/ExampleBIZCollectionViewLayout4plus1Grid/ImageCollectionViewCell.m
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.m
+//  ExampleBIZCollectionViewLayout4plus1Grid
+//
+//  Created by IgorBizi@mail.ru on 12/11/15.
+//  Copyright © 2015 IgorBizi@mail.ru. All rights reserved.
+//
+
+#import "ImageCollectionViewCell.h"
+
+
+@implementation ImageCollectionViewCell
+
+@end
